81118a70c932a7aa3c53f0982dcf2b7f49b66b23
carlh <carlhutant@gmail.com>
2022-03-31 00:34:55 +0800

    2022/03/31 00:34 random seed 固定，初步將參數設定改到 configure.py 上，存檔位置根據參數設定

--- log_record.py
+++ log_record.py
@@ -4,12 +4,12 @@ import os
 
 class LogRecoder:
     def __init__(self):
-        if os.path.exists(configure.model_dir + '/torch/log.txt'):
+        if os.path.exists(configure.ckpt_dir + 'log.txt'):
             raise RuntimeError
         else:
             pass
 
     def write(self, line):
-        f = open(configure.model_dir + '/torch/log.txt', 'a')
+        f = open(configure.ckpt_dir + 'log.txt', 'a')
         f.writelines(line + '\n')
         f.close()
