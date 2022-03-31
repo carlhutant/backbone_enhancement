2022/03/31 16:08 bug fixed, data using wrong normalization method cause training result bad

imagenet_example.py
@@ -185,7 +185,7 @@ def main_worker(gpu, ngpus_per_node, args):
         else:
             model = torch.nn.DataParallel(model).cuda()
 
-    print(model)
+    # print(model)
 
     # define loss function (criterion), optimizer, and learning rate scheduler
     criterion = nn.CrossEntropyLoss().cuda(args.gpu)