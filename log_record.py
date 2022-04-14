809a8f7e445271c1c883311dca77144d87f2a901
carlh <carlhutant@gmail.com>
2022-04-14 17:14:35 +0800

    2022/04/14 17:00 現在會自動產生 ckpt dir

--- log_record.py
+++ log_record.py
@@ -7,7 +7,12 @@ class LogRecoder:
         if os.path.exists(configure.ckpt_dir + 'log.txt') and not resume:
             raise RuntimeError
         else:
-            pass
+            try:
+                os.makedirs(configure.ckpt_dir)
+                print('Mkdir check point directory: {}.'.format(configure.ckpt_dir))
+            except FileExistsError:
+                # print('Check point directory exist. No need for mkdir.')
+                pass
 
     def write(self, line):
         f = open(configure.ckpt_dir + 'log.txt', 'a')
