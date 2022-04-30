6e196a346c7883660d3883a69fa0c3c138edf822
cliff0917 <tcfsh210119@gmail.com>
2022-04-30 23:16:53 +0800

    2022/04/30 23:16 adjust compute_dataset_RGB_average.py

--- compute_dataset_RGB_average.py
+++ compute_dataset_RGB_average.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 
 # Dataset config #
-dataset = 'AWA2'  # AWA2, imagenet
+dataset = 'inat2021'  # AWA2, imagenet
 datatype = 'img'  # img, tfrecord, npy
 data_advance = 'none'   # color_diff_121, none, color_diff_121_abs
 data_usage = 'train'
@@ -15,7 +15,7 @@ data_usage = 'train'
 # Directory set
 dataset_dir = configure.dataset_dir
 target_directory = '{}/{}/{}/{}/{}/'.format(dataset_dir, dataset, datatype, data_advance, data_usage)
-
+print(target_directory)
 walk_generator = os.walk(target_directory)
 root, directory, _ = next(walk_generator)
 total_mean_sum = np.zeros(configure.input_channel_list[0], dtype=float)
@@ -48,7 +48,8 @@ for d in directory:
         total_mean_sum += image.mean((0, 1), dtype=float)
         total_std_sum += image.std(axis=(0, 1), dtype=float)
         image_count += 1
-        print(image_count)
+        if image_count % 100 == 0:
+            print(image_count)
 total_mean = total_mean_sum/image_count
 total_std = total_std_sum/image_count
 print(total_mean)
