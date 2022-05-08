40b9a46b9bd2b8cb04daf8985c70dd9e3588b6c1
carlh <carlhutant@gmail.com>
2022-05-08 21:48:19 +0800

    2022/05/08 21:47 log2excel.py 稍微調整輸出格式

--- log2excel.py
+++ log2excel.py
@@ -2,7 +2,7 @@ import os
 
 log_dir = 'E:\\Model\\torch\\log.txt'
 f = open(log_dir, 'r')
-print('train\tval')
+print('train-val')
 train_bool = False
 val_bool = False
 line = f.readline()
@@ -12,13 +12,17 @@ while line != '':
             raise RuntimeError
         train_bool = True
         train = line[-8:-2]
+        if train[0] == ':':
+            train = train[1:]
     if 'val' in line:
         if not train_bool:
             raise RuntimeError
         val_bool = True
         val = line[-8:-2]
+        if val[0] == ':':
+            val = val[1:]
     if train_bool and val_bool:
-        print('{}\t{}'.format(train, val))
+        print('{}-{}'.format(float(train), float(val)))
         train_bool = False
         val_bool = False
     line = f.readline()
