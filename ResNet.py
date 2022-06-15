6d6260c3c0f4d4843147ce0f57a375bed644be09
carlh <carlhutant@gmail.com>
2022-06-16 03:45:04 +0800

    2022/06/16 03:32 修正 last_conv_channel_1024 identity path 需要 down sample

--- ResNet.py
+++ ResNet.py
@@ -282,10 +282,15 @@ class ResNet(nn.Module):
                 )
             )
         if last_layer and 'last_conv_channel_1024' in configure.model_mode[self.model_id]:
+            downsample = nn.Sequential(
+                conv1x1(planes * block.expansion, 1024, stride),
+                norm_layer(1024),
+            )
             layers.append(
                 block(
                     self.inplanes,
                     planes,
+                    downsample=downsample,
                     groups=self.groups,
                     base_width=self.base_width,
                     dilation=self.dilation,
